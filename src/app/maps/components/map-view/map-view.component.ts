a34c63b06bdc7e3abd5be8140d389220a22d14cf
Germán <german.palencia@adentia.es>
2023-10-11 14:37:20 +0200

    Marker and PopUp

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild,  } from '@angular/core';
 import { PlacesService } from '../../services';
-import {Map} from 'mapbox-gl';
+import {Map, Popup, Marker} from 'mapbox-gl';
 
 @Component({
   selector: 'app-map-view',
@@ -27,6 +27,17 @@ export class MapViewComponent implements AfterViewInit{
       center: this.placesService.userLocation, // starting position [lng, lat]
       zoom: 14,
     });
+
+    const popup = new Popup()
+    .setHTML(`
+      <h6>Aquí estoy</h6>
+      <span>Estoy en este lugar del mundo</span>
+    `);
+
+    new Marker({color: 'red'})
+    .setLngLat(this.placesService.userLocation)
+    .setPopup(popup)
+    .addTo(map);
 
   }
 
