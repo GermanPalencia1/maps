ef1a3d3c43a2f8c1f9f8a9f7e84e085fde129a93
Germán <german.palencia@adentia.es>
2023-10-11 15:25:37 +0200

    Bebounce

--- src/app/maps/components/search-bar/search-bar.component.ts
+++ src/app/maps/components/search-bar/search-bar.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
+
 export class SearchBarComponent {
+
+  private debounceTimer?: NodeJS.Timeout;
+
+  constructor() {}
+
+  onQueryChanged(query: string = '') {
+
+    if(this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+
+    }, 400);
+
+  }
 
 }
